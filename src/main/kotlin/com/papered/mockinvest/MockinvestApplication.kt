01a36b0d9acd1016cbe3ab9e6386027f62706e51
LeeJongHyun <dwg7691@gmail.com>
2019-05-02 09:19:01 +0900

    init

---
+++ src/main/kotlin/com/papered/mockinvest/MockinvestApplication.kt
@@ -0,0 +1,11 @@
+package com.papered.mockinvest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MockinvestApplication
+
+fun main(args: Array<String>) {
+    runApplication<MockinvestApplication>(*args)
+}
