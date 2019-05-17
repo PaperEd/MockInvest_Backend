6b954705d736b4c8082d6fcde5b598fd86977b00
LeeJongHyun <dwg7691@gmail.com>
2019-05-17 15:48:37 +0900

    필요없는 클래스 삭제

--- src/main/kotlin/com/papered/mockinvest/repository/TestRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.papered.mockinvest.repository
-
-import com.papered.mockinvest.model.Test
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface TestRepository : JpaRepository<Test, Long>
